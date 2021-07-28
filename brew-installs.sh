62db735ff3c7fb69f34596fe6a540fc289e7afd5
Harsh Desai <hrdesai007@gmail.com>
2021-07-28 00:06:00 -0500

    Update brew-installs.sh

--- brew-installs.sh
+++ brew-installs.sh
@@ -7,6 +7,7 @@ if [[ $? != 0 ]] ; then
     echo 'Installing Homebrew'
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     brew update
 fi
