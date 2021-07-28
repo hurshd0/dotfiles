1774bd43a70ab283a80ee11ce616ae4dffce5e3b
Harsh Desai <hrdesai007@gmail.com>
2021-07-27 23:52:06 -0500

    Create brew-installs.sh

---
+++ brew-installs.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+#Check if Homebrew is installed
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    echo 'Installing Homebrew'
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    brew update
+fi
+
+#Upgrade installed formulae
+brew upgrade
+
+# Save Homebrew’s installed location
+BREW_PREFIX=$(brew --prefix)
+
+# CLI Tools 
+brew install ffmpeg
+brew install rsync
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+brew install grep
+brew install node
+
+# Git, Git LFS, and Github cli
+brew install git
+brew install git-lfs
+brew install github/gh/gh
+
+# Installs Casks
+brew tap homebrew/cask
+
+#Apps I use
+# brew install --cask firefox
+# brew install --cask google-chrome
+# brew install --cask notion
+# brew install --cask spotify
+# brew install --cask vlc
+# brew install --cask visual-studio-code
+# brew install --cask zoom
+# brew install --cask discord
+# brew install --cask slack
+brew install --cask iterm2
+brew install google-backup-and-sync
+brew install google-drive
+brew install whatsapp
+
+# Remove outdated versions from the cellar.
+brew cleanup
